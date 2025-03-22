daeaf1afc43c94d5fda5fd0a63957796acc2d744
aivylv <lyvialarasati@gmail.com>
2025-03-22 12:34:21 +0800

    Menampilkan pesan error jika terjadi kesalahan eksekusi

--- CRUDsederhana/Form1.cs
+++ CRUDsederhana/Form1.cs
@@ -88,7 +88,7 @@ namespace CRUDsederhana
                 }
                 catch (Exception ex)
                 {
-                    
+                    MessageBox.Show("Error: " + ex.Message, "Kesalahan", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 }
             }
         }
