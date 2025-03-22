80c7e56faa601bd7c1b69b6b43e4f2e5dd427c34
aivylv <lyvialarasati@gmail.com>
2025-03-22 11:50:38 +0800

    Menggunakan namespace untuk manipulasi grafis

--- CRUDsederhana/Form1.cs
+++ CRUDsederhana/Form1.cs
@@ -2,5 +2,5 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
-using System.Drawing;
+using System.Drawing; //Menggunakan namespace untuk manipulasi grafis
 using System.Linq;
