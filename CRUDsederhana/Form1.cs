5b5aec7101f9e4732010ace17cb5114414f96360
aivylv <lyvialarasati@gmail.com>
2025-03-22 11:55:29 +0800

    String koneksi ke database SQL Server

--- CRUDsederhana/Form1.cs
+++ CRUDsederhana/Form1.cs
@@ -12,6 +12,7 @@ namespace CRUDsederhana
 {
     public partial class Form1: Form
     {
+        private string connectionString = "Data Source=LAPTOP-3N4S5N47\\LYVIA_SQL;" + "Initial Catalog=OrganisasiMahasiswa;Integrated Security=True";
         public Form1()
         {
             InitializeComponent();
