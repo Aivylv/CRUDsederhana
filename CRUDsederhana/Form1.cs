25b6ba5bcd3eaf8973f42d726680ed671570a96a
aivylv <lyvialarasati@gmail.com>
2025-03-22 12:45:59 +0800

    Membuat perintah SQL

--- CRUDsederhana/Form1.cs
+++ CRUDsederhana/Form1.cs
@@ -107,7 +107,10 @@ namespace CRUDsederhana
                             string nim = dgvMahasiswa.SelectedRows[0].Cells["NIM"].Value.ToString();
                             conn.Open();
                             string query = "DELETE FROM Mahasiswa WHERE NIM = @NIM";
-                            
+                            using (SqlCommand cmd = new SqlCommand(query, conn))
+                            {
+                                
+                            }
                         }
                     }
                 }
