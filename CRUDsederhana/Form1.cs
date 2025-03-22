7287597f02f052e2b6f90693616fd41cb5a450ad
aivylv <lyvialarasati@gmail.com>
2025-03-22 12:17:48 +0800

    Membuat command SQL

--- CRUDsederhana/Form1.cs
+++ CRUDsederhana/Form1.cs
@@ -66,6 +66,10 @@ namespace CRUDsederhana
                     }
                     conn.Open();
                     string query = "INSERT INTO Mahasiswa (NIM, Nama, Email, Telepon, Alamat) VALUES (@NIM, @Nama, @Email, @Telepon, @Alamat)";
+                    using (SqlCommand cmd = new SqlCommand(query, conn))
+                    {
+                        
+                    }
                 }
             }
         }
