a1fa71a7402779632f45d185595975a177a067c0
aivylv <lyvialarasati@gmail.com>
2025-03-22 12:42:32 +0800

    Membuat koneksi ke database

--- CRUDsederhana/Form1.cs
+++ CRUDsederhana/Form1.cs
@@ -100,7 +100,10 @@ namespace CRUDsederhana
                 DialogResult confirm = MessageBox.Show("Yakin ingin menghapus data ini?", "Konfirmasi", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
                 if (confirm == DialogResult.Yes)
                 {
-                    
+                    using (SqlConnection conn = new SqlConnection(connectionString))
+                    {
+                        
+                    }
                 }
             }
         }
