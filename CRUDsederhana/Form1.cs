df839275778e7d65f0784d4281c91d08ba3a55dd
aivylv <lyvialarasati@gmail.com>
2025-03-22 13:20:18 +0800

    Mengisi parameter query dengan nilai dari input pengguna (dengan Trim() untuk menghapus spasi di awal dan akhir)

--- CRUDsederhana/Form1.cs
+++ CRUDsederhana/Form1.cs
@@ -144,6 +144,7 @@ namespace CRUDsederhana
                     string query = "UPDATE Mahasiswa SET Nama = @Nama, Email = @Email, Telepon = @Telepon, Alamat = @Alamat WHERE NIM = @NIM";
                     using (SqlCommand cmd = new SqlCommand(query, conn))
                     {
+                        cmd.Parameters.AddWithValue();
                         
                     }
                 }
